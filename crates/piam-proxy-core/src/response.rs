4ca6bb70e38ac4e0624fed2627e5ea343f01d038
Cui Qiaorui <cuiqiaorui@patsnap.com>
2022-12-07 00:55:35 +0800

    feat(proxy-core): add request_id for aws_xml_error_payload

--- crates/piam-proxy-core/src/response.rs
+++ crates/piam-proxy-core/src/response.rs
@@ -40,7 +40,7 @@ impl HttpResponseExt for HttpResponse {
 impl IntoResponse for ProxyError {
     fn into_response(self) -> axum::response::Response {
         let id = Uuid::new_v4().to_string();
-        let r_t = |resp_fn: fn(&str, &str) -> HttpResponse, msg, err_type| {
+        let r_t = |resp_fn: fn(&str, &str, &str) -> HttpResponse, msg, err_type| {
             let trace_info = format!(
                 "proxy_type: {}, proxy_cluster_env: {}, \
                 error_type: {}, message: {}, x-patsnap-request-id: {}",
@@ -50,7 +50,7 @@ impl IntoResponse for ProxyError {
                 msg,
                 id
             );
-            (resp_fn(err_type, &trace_info), trace_info)
+            (resp_fn(err_type, &trace_info, &id), trace_info)
         };
         let res = match &self {
             ProxyError::BadRequest(msg)
@@ -84,36 +84,36 @@ impl IntoResponse for ProxyError {
 }
 
 pub fn rejected_by_policy() -> HttpResponse {
-    forbidden("RejectedByPolicy", "RejectedByPolicy")
+    forbidden("RejectedByPolicy", "RejectedByPolicy", &Uuid::new_v4().to_string())
 }
 
-pub fn bad_request(code: &str, message: &str) -> HttpResponse {
+pub fn bad_request(code: &str, message: &str, request_id: &str) -> HttpResponse {
     Response::builder()
         .status(StatusCode::BAD_REQUEST)
         .header(CONTENT_TYPE, "application/xml")
-        .body(Body::from(error_payload(code, message)))
+        .body(Body::from(error_payload(code, message, request_id)))
         .expect("build bad_request response should not fail")
 }
 
-pub fn forbidden(code: &str, message: &str) -> HttpResponse {
+pub fn forbidden(code: &str, message: &str, request_id: &str) -> HttpResponse {
     Response::builder()
         .status(StatusCode::FORBIDDEN)
         .header(CONTENT_TYPE, "application/xml")
-        .body(Body::from(error_payload(code, message)))
+        .body(Body::from(error_payload(code, message, request_id)))
         .expect("build forbidden response should not fail")
 }
 
-pub fn internal_err(code: &str, message: &str) -> HttpResponse {
+pub fn internal_err(code: &str, message: &str, request_id: &str) -> HttpResponse {
     Response::builder()
         .status(StatusCode::INTERNAL_SERVER_ERROR)
         .header(CONTENT_TYPE, "application/xml")
-        .body(Body::from(error_payload(code, message)))
+        .body(Body::from(error_payload(code, message, request_id)))
         .expect("build internal error response should not fail")
 }
 
-fn error_payload(code: &str, message: &str) -> String {
+fn error_payload(code: &str, message: &str, request_id: &str) -> String {
     #[cfg(feature = "aws-xml-response")]
-    aws_xml_error_payload(code, message)
+    aws_xml_error_payload(code, message, request_id)
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -124,14 +124,16 @@ pub struct AwsErrorXml {
     pub message: String,
     #[serde(rename = "AWSAccessKeyId")]
     pub aws_access_key_id: String,
+    pub request_id: String,
     pub host_id: String,
 }
 
-fn aws_xml_error_payload(code: &str, message: &str) -> String {
+fn aws_xml_error_payload(code: &str, message: &str, request_id: &str) -> String {
     let error = AwsErrorXml {
         code: format!("Piam{}", code),
-        message: format!("[Patsnap IAM] {}", message),
+        message: format!("PIAM {}", message),
         aws_access_key_id: "".into(),
+        request_id: request_id.into(),
         host_id: "".into(),
     };
     serde_xml_rs::to_string(&error).expect("ser_xml should not fail")
