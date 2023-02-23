2d74fce9b56f55d6f87fac523acdd4842b060c30
Cui Qiaorui <cuiqiaorui@patsnap.com>
2023-02-23 22:58:47 +0800

    fix(s3-proxy): remove all possible panics from request.rs

--- crates/s3-proxy/src/request.rs
+++ crates/s3-proxy/src/request.rs
@@ -1,3 +1,4 @@
+use busylib::prelude::EnhancedUnwrap;
 use http::{header::HOST, uri::PathAndQuery, HeaderValue, Uri};
 use piam_proxy::{
     error::{ProxyError, ProxyResult},
@@ -17,14 +18,7 @@ pub trait S3RequestTransform {
 
 impl S3RequestTransform for HttpRequest {
     fn adapt_path_style(&mut self, path: String, proxy_hosts: &[String]) -> ProxyResult<()> {
-        let host = self
-            .headers()
-            .get(HOST)
-            .unwrap()
-            .to_str()
-            .unwrap()
-            .to_string();
-
+        let host = self.get_host()?.to_string();
         if proxy_hosts.contains(&host) {
             // get content of path before first '/'
             let bucket = path.split('/').next().ok_or_else(|| {
@@ -36,7 +30,7 @@ impl S3RequestTransform for HttpRequest {
                 .uri_mut()
                 .path_and_query()
                 .ok_or_else(|| {
-                    ProxyError::MalformedProtocol("path_and_query should not be None".to_string())
+                    ProxyError::MalformedProtocol("path_and_query should not be empty".to_string())
                 })?
                 .as_str()
                 .strip_prefix(&format!("/{}", bucket))
@@ -50,21 +44,23 @@ impl S3RequestTransform for HttpRequest {
                 uri_without_bucket = "/";
             }
             *self.uri_mut() = Uri::builder()
-                .path_and_query(PathAndQuery::try_from(uri_without_bucket).unwrap())
+                .path_and_query(PathAndQuery::try_from(uri_without_bucket).map_err(|_| {
+                    ProxyError::MalformedProtocol(format!(
+                        "path_and_query should be valid, but got {}",
+                        uri_without_bucket
+                    ))
+                })?)
                 .build()
-                .unwrap();
+                .unwp();
 
             // add bucket to host
-            self.headers_mut().insert(
-                HOST,
-                HeaderValue::from_str(format!("{}.{}", bucket, host).as_str()).unwrap(),
-            );
+            self.set_host(&format!("{}.{}", bucket, host))?;
         }
         Ok(())
     }
 
     fn set_actual_host(&mut self, config: &S3Config, region: &str) -> ProxyResult<()> {
-        let host = self.headers().get(HOST).unwrap().to_str().unwrap();
+        let host = self.get_host()?;
         let proxy_host = config
             .proxy_hosts
             .find_proxy_host(host)
@@ -73,12 +69,46 @@ impl S3RequestTransform for HttpRequest {
             ProxyError::InvalidEndpoint(format!("host {} should end with {}", host, proxy_host))
         })?;
         let actual_host = from_region_to_host(region)?;
-        let host = format!("{}{}", bucket_dot, actual_host);
-        self.headers_mut()
-            .insert(HOST, HeaderValue::from_str(&host).unwrap());
+        self.set_host(&format!("{}{}", bucket_dot, actual_host))?;
 
         let uri = format!("http://{}{}", actual_host, self.uri());
-        *self.uri_mut() = Uri::try_from(uri).unwrap();
+        *self.uri_mut() = Uri::try_from(uri)
+            .map_err(|e| ProxyError::MalformedProtocol(format!("uri not valid: {}", e)))?;
         Ok(())
     }
+}
+
+trait HostGetterSetter {
+    fn get_host(&self) -> ProxyResult<&str>;
+    fn set_host(&mut self, host: &str) -> ProxyResult<()>;
+}
+
+impl HostGetterSetter for HttpRequest {
+    fn get_host(&self) -> ProxyResult<&str> {
+        self.headers()
+            .get(HOST)
+            .ok_or_else(host_should_not_be_empty)?
+            .to_str()
+            .map_err(|_| host_should_be_visible_ascii())
+    }
+
+    fn set_host(&mut self, host: &str) -> ProxyResult<()> {
+        self.headers_mut().insert(
+            HOST,
+            HeaderValue::from_str(host).map_err(|_| {
+                ProxyError::MalformedProtocol(format!("host should be valid, but got {}", host))
+            })?,
+        );
+        Ok(())
+    }
+}
+
+#[inline]
+fn host_should_not_be_empty() -> ProxyError {
+    ProxyError::MalformedProtocol("host should not be empty".to_string())
+}
+
+#[inline]
+fn host_should_be_visible_ascii() -> ProxyError {
+    ProxyError::MalformedProtocol("host should be visible_ascii".to_string())
 }
